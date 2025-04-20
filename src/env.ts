5d7605e9ae22b534dbf423747abb60af61dbfdae
Farhan <nahidahmed9933@gmail.com>
2025-04-20 12:56:02 +0600

    fix: CLOUDINARY_URL removed from env

--- src/env.ts
+++ src/env.ts
@@ -40,7 +40,6 @@ const EnvSchema = z.object({
   CLOUDINARY_CLOUD_NAME: z.string(),
   CLOUDINARY_API_KEY: z.string(),
   CLOUDINARY_API_SECRET: z.string(),
-  CLOUDINARY_URL: z.string(),
 });
 
 // eslint-disable-next-line node/no-process-env
