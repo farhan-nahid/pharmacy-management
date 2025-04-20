5fe7ea1b94d0f88eaade7f9138a24e7657e683e3
Farhan <nahidahmed9933@gmail.com>
2025-04-20 21:49:14 +0600

    fix: cors fix

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { cors } from "hono/cors";
 
 import { configureOpenApi } from "./lib/configure-open-api";
 import { createApp } from "./lib/create-app";
+// Route modules
 import auth from "./module/auth";
 import brand from "./module/brand";
 import category from "./module/category";
@@ -9,15 +10,10 @@ import email from "./module/email";
 import group from "./module/group";
 import index from "./module/index.route";
 import product from "./module/product";
-// Import all routes
 import upload from "./module/upload";
 
 const app = createApp();
 configureOpenApi(app);
-
-const routes = [index, auth, brand, category, group, product, email, upload] as const;
-
-routes.forEach(route => app.route("/api", route));
 
 app.use(
   "*",
@@ -31,6 +27,8 @@ app.use(
   }),
 );
 
-export type AppType = typeof routes[number];
+const routes = [index, auth, brand, category, group, product, email, upload] as const;
+routes.forEach(route => app.route("/api", route));
 
+export type AppType = typeof routes[number];
 export { app };
